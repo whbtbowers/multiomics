b9a91d7cf6d019c97e4513340704c7d957c0b361
whbtbowers <33463737+whbtbowers@users.noreply.github.com>
2018-07-10 12:22:57 +0100

    Add files via upload

--- feature_selection/g_ex/sim_net.R
+++ feature_selection/g_ex/sim_net.R
@@ -1,6 +1,7 @@
 setwd("/project/home17/whb17/Documents/project3/project_files/feature_selection/g_ex/")
 
 library(heatmap3)
+library(SNFtool)
 
 df.gene.data<- read.csv("../../data/gene_ex2/gene_test_train.csv", header=TRUE, row.names = 1)
 df.gene.data.body <- df.gene.data[,-c(1:7)]
@@ -18,19 +19,30 @@ for (i in 1:nrow(df.gene.data)){
 df.gene.data.body.hiv_neg <- df.gene.data.body[ind.hiv_neg,]
 df.gene.data.hiv_neg <- df.gene.data[ind.hiv_neg,]
 
-diff.gene.hiv_neg <- dist(df.gene.data.body.hiv_neg)
-diff.gene.hiv_neg <- as.matrix(diff.gene.hiv_neg)
+diff.gene.hiv_neg <- as.matrix(dist(df.gene.data.body.hiv_neg))
+
+AffMat.gene.hiv_neg <- affinityMatrix(diff.gene.hiv_neg, K=10)
 
 #Create and save heatmap
 
-png("img/2018-07-09/diff_mat_hmap.png",
+png("img/2018-07-10/gene_affmat_mat_hmap1.png",
     width = 3000,        # 5 x 300 pixels
     height = 3000,
     res = 300,            # 300 pixels per inch
-    pointsize = 4)        # smaller font size
+    pointsize = 8)        # smaller font size
 
-heatmap(diff.gene.hiv_neg,
+#heatmap3(AffMat.gene.hiv_neg,
+#        margins=c(10,5),
+#        main= "HIV- patients, affinity matrix",
+#        showColDendro=FALSE,
+#        showRowDendro=FALSE
+#        )
+
+heatmap(AffMat.gene.hiv_neg,
         margins=c(10,5),
-        main= "HIV- patients, Euclidian difference matrix"
+        main= "HIV- patients, affinity matrix"
         )
+
 dev.off()
+
+
